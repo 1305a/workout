4bbf6d77debf9710c40df2c7297e7dab48edbb37
1305a <1305andrew2@gmail.com>
2021-03-30 17:35:47 +0300

    begining

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,17 @@
+const inputQuantity = document.querySelector('.input-quantity')
+const buttonUp = document.querySelector('.button-up')
+const buttonDown = document.querySelector('.button-down')
 
+buttonUp.addEventListener ('click', (event) => {
+    event.preventDefault()
+    if (inputQuantity.valueAsNumber < 5) {
+        inputQuantity.valueAsNumber += 1
+    }
+})
+
+buttonDown.addEventListener ('click', (event) => {
+    event.preventDefault()
+    if (inputQuantity.valueAsNumber > 1) {
+        inputQuantity.valueAsNumber -= 1
+    }
+})
