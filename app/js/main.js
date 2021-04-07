55b2f24e3b13a27a223743b2723848743b06be64
1305a <1305andrew2@gmail.com>
2021-04-07 18:13:57 +0300

    07 04 21

--- app/js/main.js
+++ app/js/main.js
@@ -9,17 +9,17 @@ const player4 = document.querySelector('.player4')
 const player5 = document.querySelector('.player5')
 const arrrayPlayers = [player1, player2, player3, player4, player5]
 
+const bigArr = []
+const arrTasks = ['Отжимание','Планка (мин)','Скакалка','Бицепс','Трицепс','Приседания','Колесо','Пресс','Флекс','Боевой гапак']
+const tableItems = document.querySelector('.testtable')
 
-
-
-
-function hidePlayers() {
-    for (let i = 0; i < inputQuantity.valueAsNumber; i++) {
+function hidePlayers(quantityPlayers) {
+    for (let i = 0; i < quantityPlayers; i++) {
         if (arrrayPlayers[i].classList.contains('player-hide') === true)  {
             arrrayPlayers[i].classList.remove('player-hide')
         }
     }
-    for (let i = inputQuantity.valueAsNumber; i < 5; i++) {
+    for (let i = quantityPlayers; i < 5; i++) {
         if (arrrayPlayers[i].classList.contains('player-hide') === false)  {
             arrrayPlayers[i].classList.add('player-hide')
         }
@@ -30,16 +30,78 @@ buttonUp.addEventListener ('click', (event) => {
     event.preventDefault()
     if (inputQuantity.valueAsNumber < 5) {
         inputQuantity.valueAsNumber += 1
-        hidePlayers()
+        hidePlayers(inputQuantity.valueAsNumber)
+        bigArr.length = 0
+        arrayForming (inputQuantity.valueAsNumber)
     }
+    displayTable()
 })
 
 buttonDown.addEventListener ('click', (event) => {
     event.preventDefault()
     if (inputQuantity.valueAsNumber > 1) {
         inputQuantity.valueAsNumber -= 1
-        hidePlayers()
+        hidePlayers(inputQuantity.valueAsNumber)
+        bigArr.length = 0
+        arrayForming (inputQuantity.valueAsNumber)
     }
 })
 
-hidePlayers()
+function arrayForming (quantityPlayers) {
+    bigArr.push(arrTasks)
+    for (let i=0; i<quantityPlayers; i++) {
+        const arrPlayer = []
+        for (let index = 0; index < arrTasks.length; index++) {
+                const inputItem = document.createElement('input')
+                arrPlayer.push(inputItem)
+        }
+        bigArr.push(arrPlayer)
+    }
+}
+
+function displayTable() {
+    while (tableItems.firstChild) {
+        tableItems.removeChild(tableItems.firstChild);
+      }
+
+    for (let iTen = 1; iTen < 10; iTen++) {
+        let tr = document.createElement('tr')
+        for (let i = 0; i < bigArr.length; i++) {
+            let td = document.createElement('td')
+            //let input = document.createElement('input')
+            if (i != 0) {
+                //td.innerHTML = bigArr[i][iTen]
+                tr.appendChild(td)
+                console.log(bigArr[i][iTen])
+                td.appendChild(bigArr[i][iTen])
+            }
+            else {
+                td.innerHTML = bigArr[i][iTen]
+                tr.appendChild(td)
+            }
+        }
+        tableItems.appendChild(tr)
+    }
+      
+}
+
+
+// const table = document.querySelector('.testtable')
+// const arr = ['Отжимание','Планка (мин)','Скакалка','Бицепс','Трицепс','Приседания','Колесо','Пресс','Флекс','Боевой гапак']
+// function fillTable(table,arr) {
+//     console.log(table,arr)
+//     for (let i=0; i < arr.length; i++) {
+//         let tr = document.createElement('tr')
+
+//         for (let j=0; j < arr[i].length; j++) {
+//             let td = document.createElement('td')
+//             td.innerHTML = arr[i][j]
+//             tr.appendChild(td)
+//         }
+//         table.appendChild(tr)
+//     }
+// }
+// fillTable(table,arr)
+
+hidePlayers(inputQuantity.valueAsNumber)
+arrayForming (inputQuantity.valueAsNumber)
