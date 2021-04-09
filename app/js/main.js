ef05f49f083efcfc5c317d4db7283da6af5c4269
1305a <1305andrew2@gmail.com>
2021-04-09 16:13:52 +0300

    09 04 2021

--- app/js/main.js
+++ app/js/main.js
@@ -12,13 +12,22 @@ const tdPlayer2 = document.querySelector('.td-player2')
 const tdPlayer3 = document.querySelector('.td-player3')
 const tdPlayer4 = document.querySelector('.td-player4')
 const tdPlayer5 = document.querySelector('.td-player5')
-const testButton = document.querySelector('.testbutton')
+//const testButton = document.querySelector('.testbutton')
 const arrrayPlayers = [player1, player2, player3, player4, player5]
 const arrrayTdPlayers = [tdPlayer1, tdPlayer2, tdPlayer3, tdPlayer4, tdPlayer5]
 
 const bigArr = []
-const arrTasks = ['Отжимание','Планка (мин)','Скакалка','Бицепс','Трицепс','Приседания','Колесо','Пресс','Флекс','Боевой гапак']
+const arrTasks = ['Отжимание','Планка (мин)','Скакалка','Бицепс','Трицепс','Приседания','Колесо','Пресс','Эспандер','По желанию']
 const tableItems = document.querySelector('.maintable')
+const level = document.querySelector('.level')
+const calculate = document.querySelector('.calculate')
+
+const objectPlayer1 = {'Отжимание':0,'Планка (мин)':0,'Скакалка':0,'Бицепс':0,'Трицепс':0,'Приседания':0,'Колесо':0,'Пресс':0,'Эспандер':0,'По желанию':0}
+const objectPlayer2 = {'Отжимание':0,'Планка (мин)':0,'Скакалка':0,'Бицепс':0,'Трицепс':0,'Приседания':0,'Колесо':0,'Пресс':0,'Эспандер':0,'По желанию':0}
+const objectPlayer3 = {'Отжимание':0,'Планка (мин)':0,'Скакалка':0,'Бицепс':0,'Трицепс':0,'Приседания':0,'Колесо':0,'Пресс':0,'Эспандер':0,'По желанию':0}
+const objectPlayer4 = {'Отжимание':0,'Планка (мин)':0,'Скакалка':0,'Бицепс':0,'Трицепс':0,'Приседания':0,'Колесо':0,'Пресс':0,'Эспандер':0,'По желанию':0}
+const objectPlayer5 = {'Отжимание':0,'Планка (мин)':0,'Скакалка':0,'Бицепс':0,'Трицепс':0,'Приседания':0,'Колесо':0,'Пресс':0,'Эспандер':0,'По желанию':0}
+
 
 function hidePlayers(quantityPlayers) {
     for (let i = 0; i < quantityPlayers; i++) {
@@ -59,29 +68,90 @@ buttonDown.addEventListener ('click', (event) => {
     }
 })
 
-testButton.addEventListener ('click', (event) => {
+calculate.addEventListener ('click', (event) => {
     event.preventDefault()
-    // idDoc = document.getElementById('10')
-    // console.log(bigArr)
     for (let i = 1; i < bigArr.length; i++) {
         //const element = bigArr[i];
         for (let j = 0; j < bigArr[i].length; j++) {
-            //console.log(document.getElementById(bigArr[i][j]))
-            //bigArr[i][j] = document.getElementById(bigArr[i][j])
-            bigArr[i][j] = document.getElementById(bigArr[i][j])
+            
+            if (i===1) {
+                objectPlayer1[bigArr[0][j]] = document.getElementById(bigArr[i][j]).valueAsNumber
+            }
+            if (i===2) {
+                objectPlayer2[bigArr[0][j]] = document.getElementById(bigArr[i][j]).valueAsNumber
+            }
+            if (i===3) {
+                objectPlayer3[bigArr[0][j]] = document.getElementById(bigArr[i][j]).valueAsNumber
+            }
+            if (i===4) {
+                objectPlayer4[bigArr[0][j]] = document.getElementById(bigArr[i][j]).valueAsNumber
+            }
+            if (i===5) {
+                objectPlayer5[bigArr[0][j]] = document.getElementById(bigArr[i][j]).valueAsNumber
+            }
         }
-        
     }
-    console.log(bigArr)
+    //console.log(objectPlayer1)
+    //console.log(inputQuantity.valueAsNumber)
+
+    for (let index = 1; index < inputQuantity.valueAsNumber+1; index++) {
+        
+        let finalResult = 'Выполнить '
+        if (index === 1) {
+            const finalTask1 = calculateLevels(objectPlayer1)
+            finalResult = finalResult + ' ' + finalTask1
+        }
+        else if (index === 2) {
+            const finalTask2 = calculateLevels(objectPlayer2)
+            finalResult = finalResult + ' ' + finalTask2
+        }
+        else if (index === 3) {
+            const finalTask3 = calculateLevels(objectPlayer3)
+        }
+        else if (index === 4) {
+            const finalTask4 = calculateLevels(objectPlayer4)
+        }
+        else if (index === 5) {
+            const finalTask5 = calculateLevels(objectPlayer5)
+        }
+        console.log(finalResult)
+    }
 })
+
+function calculateLevels(objectPlayer) {
+    const copyArrTask = arrTasks.slice()
+    copyArrTask.sort(function(){ 
+        return 0.5 - Math.random()
+        })
+    let finalTask = ''
+    if (level.value === 'easy') {
+        for (let index = 0; index < 3; index++) {
+            finalTask = finalTask + ' ' + copyArrTask[index] + ' ' + objectPlayer[copyArrTask[index]]
+        }
+    }
+    else if (level.value === 'normal') {
+        for (let index = 0; index < 5; index++) {
+            finalTask = finalTask + ' ' + copyArrTask[index] + ' ' + objectPlayer[copyArrTask[index]]
+        }
+    }
+    else if (level.value === 'hard') {
+        for (let index = 0; index < 7; index++) {
+            finalTask = finalTask + ' ' + copyArrTask[index] + ' ' + objectPlayer[copyArrTask[index]]
+        }
+    }
+    //console.log(finalTask)
+    return finalTask
+}
+
+
+
+
 
 function arrayForming (quantityPlayers) {
     bigArr.push(arrTasks)
     for (let i=0; i<quantityPlayers; i++) {
         const arrPlayer = []
         for (let index = 0; index < arrTasks.length; index++) {
-                //const inputItem = document.createElement('input')
-                //arrPlayer.push(inputItem)
                 arrPlayer.push(0)
         }
         bigArr.push(arrPlayer)
